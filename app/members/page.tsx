c82880ab688df06d845e2b3519adb25f38ddc936
elshaddaioheha <105729599+elshaddaioheha@users.noreply.github.com>
2025-12-19 10:07:34 +0100

    Update raised amount to 505,500

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -44,7 +44,7 @@ interface Donation {
 
 export default function MembersStats() {
     const [stats, setStats] = useState({
-        totalDonations: 404500,
+        totalDonations: 505500,
         recentDonations: [] as Donation[]
     });
 
